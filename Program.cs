25be4f9cb66ca0685818fbb79244f61d99e3027a
alepautila <ale.pautila@gmail.com>
2021-07-02 21:50:13 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace projetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+            nome = Console.ReadLine(); 
+            Console.WriteLine("Hello " + nome);
+//Console.ReadLine(); 
+        }
+    }
+}
